2dcb55c5c931f1caa4757e5825b15f4778737e14
Sakshi03022004 <99466592+Sakshi03022004@users.noreply.github.com>
2022-08-05 19:26:19 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class fact
+{
+	public static void main(String args[])
+	{
+		int n, fact=1;
+		System.out.print("enter the number:->");
+		Scanner r=new Scanner(System.in);
+		n=r.nextInt();
+		for(int i=1;i<=n; i++)
+		{
+			fact=fact*i;
+		}
+		System.out.print("factorial:->"+fact);
+	}
+}
