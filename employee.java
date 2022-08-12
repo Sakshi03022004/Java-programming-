b022f2c795ef26c694262475c02b627e36051641
Sakshi03022004 <99466592+Sakshi03022004@users.noreply.github.com>
2022-08-12 21:46:46 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,26 @@
+//Program to create class with members and methods
+import java.util.Scanner;
+class Employee
+{
+int eid;
+String ename;
+void insert()
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the eid:");
+eid=s.nextInt();
+System.out.println("Enter the ename:");
+ename=s.next();
+}
+void display()
+{
+System.out.println("Employee id is:"+eid);
+System.out.println("Employee name is:"+ename);
+}
+public static void main(String[] args)
+{
+Employee p=new Employee();
+p.insert();
+p.display();
+}
+}
