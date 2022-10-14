32e2e644985d5a90d68096f212c5b32c2eb3356c
Sakshi03022004 <99466592+Sakshi03022004@users.noreply.github.com>
2022-10-14 19:04:14 +0530

    Add files via upload

---
+++ Wrapperclass.java
@@ -0,0 +1,57 @@
+//assignment no 6 Write a java programe to demonstarte use of wrapper class
+public class Wrapperclass
+{
+	public static void main(String args[])
+	{
+		byte b=10;
+		short s=20;
+		int i=30;
+		long l=40;
+		float f=50.0f;
+		double d=60.0d;
+		char c='a';
+		boolean b2=true;
+
+		//Autoboxing:converting primitives,into objects
+		Byte byteobj=b;
+		Short shortobj=s;
+		Integer intobj=i;
+		Long longobj=l;
+		float floatobj=f;
+		Double doubleobj=d;
+		Character charobj=c;
+		Boolean boolobj=b2;
+
+		//printing objects
+		System.out.println("--printing object value--");
+		System.out.println("Byte object:"+byteobj);
+		System.out.println("Short object"+shortobj);
+		System.out.println("Integer  object"+intobj);
+		System.out.println("Long object"+longobj);
+		System.out.println("Float object"+floatobj);
+		System.out.println("Double  object"+doubleobj);
+		System.out.println("Charecter  object"+charobj);
+		System.out.println("Boolean object"+boolobj);
+
+		//Unboxing converting objects to primitives
+		byte bytevalue=byteobj;
+		short shortvalue=shortobj;
+		int intvalue=intobj;
+		long longvalue=longobj;
+		float floatvalue=floatobj;
+		double doublevalue=doubleobj;
+		char charvalue=charobj;
+		boolean boolvalue=boolobj;
+
+		//printing primitives
+		System.out.println("--printing primitive value--");
+				System.out.println("byte value:"+bytevalue);
+				System.out.println("short value"+shortvalue);
+				System.out.println("integer  value"+intvalue);
+				System.out.println("long value"+longvalue);
+				System.out.println("float value"+floatvalue);
+				System.out.println("double value"+doublevalue);
+				System.out.println("charecter  value"+charvalue);
+		System.out.println("boolean value"+boolvalue);
+	}
+}
